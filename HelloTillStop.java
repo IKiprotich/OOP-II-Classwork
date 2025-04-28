31a36628f439816172bf111274cebd5f10617fdb
Ian Kiprotich <kiprotichian254@gmail.com>
2025-04-28 11:13:02 +0300

    first commit

---
+++ HelloTillStop.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class HelloTillStop {
+    Scanner sc = new Scanner(System.in);
+
+    // Outputs hello when the user wants to
+    void showHello() {
+        String choice; // Guard variable
+
+        System.out.println("For Hello, type 'h' anything else to stop");
+
+        choice = sc.next(); // Set guard variable
+
+        while ("h".equals(choice)) { // Check guard variable
+            System.out.println("Hello");
+
+            choice = sc.next(); // Update guard variable
+        }
+    }
+
+    public static void main(String[] args) {
+        HelloTillStop helloTillStop = new HelloTillStop();
+        helloTillStop.showHello();
+    }
+}
